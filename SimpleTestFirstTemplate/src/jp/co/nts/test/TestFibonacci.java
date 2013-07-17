61157f10406100aa2f5faafa36b17e61c48370b7
eiji <eijiochiai8@yahoo.co.jp>
2013-07-17 14:24:58 +0900

    Bug Fix test code

--- SimpleTestFirstTemplate/src/jp/co/nts/test/TestFibonacci.java
+++ SimpleTestFirstTemplate/src/jp/co/nts/test/TestFibonacci.java
@@ -23,7 +23,7 @@ public class TestFibonacci {
 	@Parameters
 	public static Collection<Object[]> data() {
 		return Arrays.asList(new Object[][] { { 0, 0 }, { 1, 1 }, { 2, 1 },
-				{ 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }, { 7, 13 },  { 8, 21}, 
+				{ 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }, { 7, 13 },  { 8, 21},
 				{ 10, 55 },
 				/*
 				{ 20, 6765 }, // before:21891
@@ -37,10 +37,10 @@ public class TestFibonacci {
 		this.input = input;
 		this.output = output;
 	}
-	
+
 	@Test
 	public void testFibonacci() {
 		Fibonacci obj = new Fibonacci();
-		assertEquals(input, obj.getFibonacci(output));
+		assertEquals(output, obj.getFibonacci(input));
 	}
 }
